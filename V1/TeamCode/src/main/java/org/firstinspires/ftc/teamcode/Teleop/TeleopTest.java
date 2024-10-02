e7d536ddad50d783adc0286d9c23502c2ded7169
John Moorcroft <DerTeleporterNeun@outlook.com>
2024-10-02 09:59:46 +0200

    Adding first teleop Class for testing purposes

---
+++ V1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/TeleopTest.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.Teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+//Bitte gebrauch git, ich weis dass du es nich lernen wirst nico....
+
+public class TeleopTest extends OpMode {
+
+    String initMessage = "The code has been initialized";
+    @Override
+    public void init() {
+        telemetry.addData("Title", initMessage); //Dies ist nur ein test code, zu sehen ob es funkt
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
